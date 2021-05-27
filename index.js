c88261b89ea4915cd3e6424fb2e73297d26da9d1
Radensuaso <andrels9283@gmail.com>
2021-05-27 14:18:47 +0100

    Function to generate the numbers implemented

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const createNumbers = function () {}
+window.onload = function () {
+  createNumbers()
+}
+
+// function to create and deploy numbers on the page
+const createNumbers = function () {
+  const numberContainer = document.querySelector("#number-container")
+  for (let i = 1; i <= 76; i++) {
+    const newNumber = document.createElement("div")
+    newNumber.innerText = i
+    newNumber.classList.add("numbers")
+    numberContainer.appendChild(newNumber)
+  }
+}
